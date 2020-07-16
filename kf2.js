c0302c3ccc96eb0fda064cb0124afc5033067f64
defunsm <salmanhossain500@gmail.com>
2020-07-16 17:23:43 -0400

    kf2 login

--- kf2.js
+++ kf2.js
@@ -1,8 +1,12 @@
 const puppeteer = require('puppeteer');
-const { exec, spawn } = require('child_process');
+const Apify = require('apify');
+
+const { exec } = require('child_process');
 require('dotenv').config();
 
 const SERVER_URL = 'http://localhost:8080';
+const KF2_USERNAME = process.env.KF2_USERNAME;
+const KF2_PASSWORD = process.env.KF2_PASSWORD;
 
 module.exports = {
     startServer: async function (msg, embed) {
@@ -14,6 +18,12 @@ module.exports = {
             const browser = await puppeteer.launch({defaultViewport: {width: 1000, height: 650}});
             const page = await browser.newPage();
             await page.goto(`${SERVER_URL}`);
+            // login
+            await page.type(`${KF2_USERNAME}`, input.username);
+            await page.type(`${KF2_PASSWORD}`, input.password);
+            await page.click('#loginbutton input');
+            await page.waitForNavigation();
+
             await page.screenshot({path: 'example.png'});
         }
         
